c3044e9c93f7c6553684811dca1480e15b77e6a5
maykon.elias <maykon.elias@hhsurge.com>
2020-01-09 12:52:14 +0000

    Initial commit

---
+++ app/src/main/java/com/mykoelias/sorteio/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mykoelias.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarBotao(View v) {
+
+        int x = new Random().nextInt(11); // 0 1 2 3 4 5 6 7 8 9 10
+        TextView texto = findViewById(R.id.textoResultado);
+        texto.setText("O número sorteado foi: " + x);
+
+    }
+}
